7406a45ff0d71b4c40c56adc8da4afd7a1050ff0
turtlesbro <zakthompson90@gmail.com>
2017-09-08 10:49:37 -0700

    Python stufff

--- 9-8HW/problem 29.py
+++ 9-8HW/problem 29.py
@@ -1,6 +1,6 @@
 import turtle
 
-def createLSystem(numIters,axiom):
+def createLSystem(numIters, axiom):
     startString = axiom
     endString = ""
     for i in range(numIters):
@@ -18,40 +18,44 @@ def processString(oldStr):
 
 def applyRules(ch):
     newstr = ""
-    if ch ==  'Y':
-        newstr = 'F--F---F+++j+++F---'
-    elif ch ==  'K':
-        newstr = 'YYYYYY++FFjK'
+    if ch == 'F':
+        newstr = 'F[-F]F[+F]F'   # Rule 1
     else:
-        newstr = ch    # no rules apply so keep the character
+        newstr = ch     # no rules apply so keep the character
 
     return newstr
 
 def drawLsystem(aTurtle, instructions, angle, distance):
+    savedInfoList = []
     for cmd in instructions:
         if cmd == 'F':
             aTurtle.forward(distance)
-        elif cmd == 'j':
-            aTurtle.forward(3)
         elif cmd == 'B':
             aTurtle.backward(distance)
         elif cmd == '+':
             aTurtle.right(angle)
         elif cmd == '-':
             aTurtle.left(angle)
+        elif cmd == '[':
+            savedInfoList.append([aTurtle.heading(), aTurtle.xcor(), aTurtle.ycor()])
+            #print(savedInfoList)
+        elif cmd == ']':
+            newInfo = savedInfoList.pop()
+            aTurtle.setheading(newInfo[0])
+            aTurtle.setposition(newInfo[1], newInfo[2])
+
 
 def main():
-    inst = createLSystem(250, "K")   # create the string
+    inst = createLSystem(6, "F")   # create the string
     print(inst)
     t = turtle.Turtle()            # create the turtle
     wn = turtle.Screen()
-
     t.up()
-    t.back(2)
+    t.back(200)
     t.down()
     t.speed(9)
-    drawLsystem(t, inst, 30, 20)   # draw the picture
-                                  # angle 60, segment length 5
+    drawLsystem(t, inst, 25, 5)  # draw the picture
+
     wn.exitonclick()
 
 main()
