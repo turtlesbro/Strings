a7070e5e597a665c5383202cd8ca1bea0f0bb954
turtlesbro <zakthompson90@gmail.com>
2017-09-20 13:18:16 -0700

    Python stufff

--- 9-18/tarea.py
+++ 9-18/tarea.py
@@ -1,24 +1,19 @@
 def countAll(text):
-    d = {}
     text.lower()
     for letter in text:
         d[letter] = text.count(letter)
     return d
-d = countAll('Banana boats')
-g =[]
+d = {}
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-for i in d:
-    g = d
+for letter in alphabet:
+    d[letter] = 0
+d = countAll('Banana boats are low key snipez on the potato')
 import turtle
 t = turtle.Turtle()
 wn = turtle.Screen()
 wn.setworldcoordinates(-20, -20, 500, 500)
 t.speed(0)
 t.fd(500)
-print(d)
-for letter in alphabet:
-    if d[letter] == 'null':
-        d[letter] = 0
 t.backward(500)
 for i in range(26):
     t.fd(15)
@@ -30,13 +25,32 @@ t.backward(390)
 t.left(90)
 t.fd(500)
 t.backward(500)
-t.speed(1)
-print(g)
+t.right(90)
+t.pensize(5)
+t.penup()
+t.right(90)
+t.fd(10)
 
+t.left(90)
+t.fd(7)
 for letter in alphabet:
-    if d[letter] > 0:
-        print(d[letter])
-    else:
-        d=d
+    t.write(letter)
+    t.fd(15)
+t.backward(397)
+t.left(90)
+t.fd(10)
+t.right(90)
+t.pendown()
+t.fd(3)
+for letter in alphabet:
+    t.left(90)
+    t.fd(d[letter]*20)
+    t.right(90)
+    t.fd(9)
+    t.right(90)
+    t.fd(d[letter]*20)
+    t.left(90)
+    t.fd(6)
+t.speed(1)
 #if countAll():
 t.fd(30300)
