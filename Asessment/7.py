80dede580e943c02b1e0ad70ae952cb177d05d73
turtlesbro <zakthompson90@gmail.com>
2017-09-25 17:16:03 -0700

    Python stufff

--- Asessment/7.py
+++ Asessment/7.py
@@ -1,13 +1,16 @@
 text =  open("her-name-is-alice", "r")
 text = text.read()
-text = str(text)
+
 d = {}
 g = []
-for i in range(3738):
-    for aline in text:
+word = ''
+for aline in text:
+    letter = aline
+    if letter!= ' ':
+            word = word + letter
+    else:
+        d[word] = text.count(word)
+        word = ''
 
-        word = aline.split()
-        for a in range(len(word)):
-            d[word[a-1]] = text.count(word[a-1])
-g.append(d)
-print(g)
+print(d)
+
